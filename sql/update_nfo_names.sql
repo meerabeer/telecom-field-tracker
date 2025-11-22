d009cce25cfc5cd5b464ca46457a123f3ce65b7f
meerabeer <muhammad_abeer@outlook.com>
2025-11-22 21:42:33 +0300

    Restore provided user and site data lists

--- sql/update_nfo_names.sql
+++ sql/update_nfo_names.sql
@@ -1,93 +1,185 @@
 -- Run this script in Supabase SQL editor to sync nfo_status.name with NFOusers.csv.
 
 update nfo_status
-set name = 'ANAYA'
-where id = 'ANAYA';
+-- Sync nfo_status.name with NFOusers list
+-- Run this once in Supabase SQL editor
 
-update nfo_status
-set name = 'BASHIR'
-where id = 'BASHIR';
-
-update nfo_status
-set name = 'DANISH_JAVED'
-where id = 'DANISH_JAVED';
-
-update nfo_status
-set name = 'Pratap'
+update nfo_status set name = 'Pratap'
 where id = 'EZLAKCX';
 
-update nfo_status
-set name = 'FATIMA'
-where id = 'FATIMA';
-
-update nfo_status
-set name = 'JAVERIA'
-where id = 'JAVERIA';
-
-update nfo_status
-set name = 'KHAN'
-where id = 'KHAN';
-
-update nfo_status
-set name = 'MALIK'
-where id = 'MALIK';
-
-update nfo_status
-set name = 'MUHAMMAD'
-where id = 'MUHAMMAD';
-
-update nfo_status
-set name = 'NAYAB'
-where id = 'NAYAB';
-
-update nfo_status
-set name = 'nfo1'
-where id = 'nfo1';
-
-update nfo_status
-set name = 'nfo2'
-where id = 'nfo2';
-
-update nfo_status
-set name = 'NIMRA'
-where id = 'NIMRA';
-
-update nfo_status
-set name = 'NOOR'
-where id = 'NOOR';
-
-update nfo_status
-set name = 'REHMAN'
-where id = 'REHMAN';
-
-update nfo_status
-set name = 'RIZWAN'
-where id = 'RIZWAN';
-
-update nfo_status
-set name = 'SAMAN'
-where id = 'SAMAN';
-
-update nfo_status
-set name = 'SHIFA'
-where id = 'SHIFA';
-
-update nfo_status
-set name = 'Salman Ashref RaJa Mohammad Ashref'
+update nfo_status set name = 'Salman Ashref RaJa Mohammad Ashref'
 where id = 'ZAASLJR';
 
-update nfo_status
-set name = 'Tabassum Rana'
+update nfo_status set name = 'Tabassum Rana'
 where id = 'ZABBTAB';
 
-update nfo_status
-set name = 'Abdul Mateen Ghulam Mustafa'
+update nfo_status set name = 'Abdul Mateen Ghulam Mustafa'
 where id = 'ZABDGHU';
 
-update nfo_status
-set name = 'Ahmed Samir AbdelSadik Ghonimy'
+update nfo_status set name = 'Ahmed Samir AbdelSadik Ghonimy'
 where id = 'ZADAHBM';
 
-update nfo_status
-set name = 'ZAIN'
-where id = 'ZAIN';
+update nfo_status set name = 'Ahmed Othman Fares Othman'
+where id = 'ZAHFARM';
+
+update nfo_status set name = 'Amir Hussain Anwer Hussain'
+where id = 'ZAINMWA';
+
+update nfo_status set name = 'Naveed Akram Sabir M Sabir'
+where id = 'ZAKRNAV';
+
+update nfo_status set name = 'Amjad Farooq'
+where id = 'ZALLAMJ';
+
+update nfo_status set name = 'NoorUl Ameen Jainulabudeen Ansari'
+where id = 'ZANSNOO';
+
+update nfo_status set name = 'Salman Ashraf Raja Muhammad Ashraf'
+where id = 'ZASHSAL';
+
+update nfo_status set name = 'Asjad Javed'
+where id = 'ZASJJAV';
+
+update nfo_status set name = 'Tariq Hindawe'
+where id = 'ZATSRHA';
+
+update nfo_status set name = 'Yasir Abdalhfiz Mohamed Abuzeid'
+where id = 'ZAYSHMO';
+
+update nfo_status set name = 'AbuBakar Babiker'
+where id = 'ZBABABU';
+
+update nfo_status set name = 'Bilal Saleem'
+where id = 'ZBILSAL';
+
+update nfo_status set name = 'Asad Maqsood Butt Maqsood Akhter Butt'
+where id = 'ZBUTASA';
+
+update nfo_status set name = 'Faris Shah Fayyaz'
+where id = 'ZCRUDEN';
+
+update nfo_status set name = 'Hossam Ahmed abdallah Eltoukhy'
+where id = 'ZDSHBOA';
+
+update nfo_status set name = 'Tabassum Abas Ghulam Abbas Rana'
+where id = 'ZGHUTAB';
+
+update nfo_status set name = 'Vengateshan G'
+where id = 'ZGNXEVX';
+
+update nfo_status set name = 'Ahmed Rahman Gul Roz khan'
+where id = 'ZGULAHM';
+
+update nfo_status set name = 'Mahmood Saeed'
+where id = 'ZHAASME';
+
+update nfo_status set name = 'Muhammad Shakeel Abdul Karim'
+where id = 'ZHDMBAU';
+
+update nfo_status set name = 'Zahid Aslam'
+where id = 'ZHMUAZH';
+
+update nfo_status set name = 'Muhammad Umair Muhammad Ashraf'
+where id = 'ZHMUUHM';
+
+update nfo_status set name = 'Muhammad Shakeel Abdul Karim'
+where id = 'ZHUMABD';
+
+update nfo_status set name = 'Muqadar Hussain'
+where id = 'ZHUSMUQ';
+
+update nfo_status set name = 'Kashif Iqbal Waris Khan'
+where id = 'ZKAASWR';
+
+update nfo_status set name = 'MD Nazmul Islam'
+where id = 'ZLDMSIN';
+
+update nfo_status set name = 'Muhammad Faizan A.M.Akhtar Khan'
+where id = 'ZMAHUAM';
+
+update nfo_status set name = 'Abdul Mateen Mustafa'
+where id = 'ZMATMUS';
+
+update nfo_status set name = 'Muhammad Nasir Masood Masood Ahmad'
+where id = 'ZMAUSHM';
+
+update nfo_status set name = 'Muhammad Ibrar Muhammad Mushtaq'
+where id = 'ZMDMSIA';
+
+update nfo_status set name = 'Muhammad Faizan A.M.Akhtar Khan'
+where id = 'ZMFARAR';
+
+update nfo_status set name = 'Amar Shahzad Muhammad Tahir Chaudhry'
+where id = 'ZMHAAUM';
+
+update nfo_status set name = 'Ahmed Mohsen Zaki'
+where id = 'ZMMHZOA';
+
+update nfo_status set name = 'Muhammad Waris Inyat Ali'
+where id = 'ZMUHINY';
+
+update nfo_status set name = 'Sajjad Ahmed Muhammad Saleem'
+where id = 'ZMUHSAJ';
+
+update nfo_status set name = 'Muhammad Ibrar Mushtaq'
+where id = 'ZMUMHHU';
+
+update nfo_status set name = 'Amir Hussain Anwer Hussain'
+where id = 'ZNAIMAW';
+
+update nfo_status set name = 'Mohamed Yasser'
+where id = 'ZOGDSMR';
+
+update nfo_status set name = 'Zain Afzal Mohammed'
+where id = 'ZOMHAIZ';
+
+update nfo_status set name = 'Muhammad Ali Qureshi Asif Qureshi'
+where id = 'ZQURMUH';
+
+update nfo_status set name = 'Rafi Ul Haq Abdul hamid Khan'
+where id = 'ZRDABFA';
+
+update nfo_status set name = 'Ahmed Rahman Gul Roz khan'
+where id = 'ZROQABD';
+
+update nfo_status set name = 'Muhammad Umer Raza'
+where id = 'ZRZUAHM';
+
+update nfo_status set name = 'Naveed Akram Sabir M Sabir'
+where id = 'ZSABNAV';
+
+update nfo_status set name = 'Elsayed Ibrahim ElSayed'
+where id = 'ZSAIELS';
+
+update nfo_status set name = 'Shazad Sabir'
+where id = 'ZSBSAHA';
+
+update nfo_status set name = 'Amar Shahzad  Muhammad Tahir Chaudhry'
+where id = 'ZSHAAME';
+
+update nfo_status set name = 'Salman Aslam'
+where id = 'ZSMALUH';
+
+update nfo_status set name = 'Usman Muhammad'
+where id = 'ZSUMMHU';
+
+update nfo_status set name = 'Muhammad Naveed Amin'
+where id = 'ZUMNAVH';
+
+update nfo_status set name = 'Muhammad Kashif Shahzad'
+where id = 'ZUMSHHA';
+
+update nfo_status set name = 'Muhammad Ali Qureshi Asif Qureshi'
+where id = 'ZUQUMHR';
+
+update nfo_status set name = 'Muhammad Waris Inyat Ali'
+where id = 'ZURRHAF';
+
+update nfo_status set name = 'Muhammad Syed Farrukh'
+where id = 'ZYFASRE';
+
+update nfo_status set name = 'Syed Muneeb Kareem Syed Mujeeb Ul Kareem'
+where id = 'ZYSSEEY';
+
+update nfo_status set name = 'Hamid Raza Hafiz Muhammad Ali'
+where id = 'ZZHMRAA';
